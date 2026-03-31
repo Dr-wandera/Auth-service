559897fc603c370f767a3243f1354a78aa5bde16
Dr-wandera <wanderafaustine@gmail.com>
2026-03-31 09:07:43 +0300

    updated repository

--- src/main/java/com/wanderaTech/auth_service/Repository/OtpVerificationRepository.java
+++ src/main/java/com/wanderaTech/auth_service/Repository/OtpVerificationRepository.java
@@ -1,9 +1,17 @@
 package com.wanderaTech.auth_service.Repository;
 
 import com.wanderaTech.auth_service.Model.OtpVerification;
+import com.wanderaTech.auth_service.Model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OtpVerificationRepository extends JpaRepository<OtpVerification,Long> {
+    void deleteByUser(Users user);
+
+   Optional<OtpVerification> findTopByUserOrderByExpiryTimeDesc(Users user);
+
+    Optional<OtpVerification> findByOtpCode(String otpCode);
 }
