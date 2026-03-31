3347a3ef74d1904e1b438daeeeb44a41ba4fee5a
Dr-wandera <wanderafaustine@gmail.com>
2026-03-31 09:02:25 +0300

    added correct imports

--- src/main/java/com/wanderaTech/auth_service/Security/AppUserDetailService.java
+++ src/main/java/com/wanderaTech/auth_service/Security/AppUserDetailService.java
@@ -3,13 +3,11 @@ package com.wanderaTech.auth_service.Security;
 import com.wanderaTech.auth_service.Model.Users;
 import com.wanderaTech.auth_service.Repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,7 +16,7 @@ public class AppUserDetailService implements UserDetailsService {
     private final UsersRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(@NonNull String email) throws UsernameNotFoundException {
 
         Users user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
