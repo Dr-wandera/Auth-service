78220dc3b3a500fe8171982fc366588b58623dcf
Dr-wandera <wanderafaustine@gmail.com>
2026-03-31 09:04:44 +0300

    added userId method

--- src/main/java/com/wanderaTech/auth_service/Controller/AuthController.java
+++ src/main/java/com/wanderaTech/auth_service/Controller/AuthController.java
@@ -1,25 +1,39 @@
 package com.wanderaTech.auth_service.Controller;
 
 import com.wanderaTech.auth_service.AuthDto.LoginRequest;
+import com.wanderaTech.auth_service.AuthDto.OtpRequest;
 import com.wanderaTech.auth_service.AuthDto.RegisterRequest;
 import com.wanderaTech.auth_service.Service.AuthServiceImplementation;
+import com.wanderaTech.auth_service.Service.OtpVerificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.MessagingException;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthServiceImplementation authServiceImplementation;
+    private final OtpVerificationService otpVerificationService;
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public String createUser(@RequestBody RegisterRequest registerRequest){
-        return authServiceImplementation.registerUser(registerRequest);
+    public ResponseEntity<?> createUser(@RequestBody RegisterRequest registerRequest){
+         authServiceImplementation.registerUser(registerRequest);
+        return ResponseEntity.ok(
+                Map.of(
+                        "message", "User registered successfully. Check your email for OTP verification.",
+                        "email", registerRequest.getEmail()
+                )
+        );
     }
     @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest){
         return authServiceImplementation.login(loginRequest);
     }
--- src/main/java/com/wanderaTech/auth_service/Security/CustomUserDetails.java
+++ src/main/java/com/wanderaTech/auth_service/Security/CustomUserDetails.java
@@ -15,6 +15,10 @@ public class CustomUserDetails implements UserDetails {
 
     private final Users user;
 
+  public  String getUserId(){
+      return user.getUserId();
+  }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority("ROLE_"+user.getRole().name()));
