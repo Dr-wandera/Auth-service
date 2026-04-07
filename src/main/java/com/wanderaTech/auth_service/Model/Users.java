803584b842bbefa02b2c621ba9ef42ad481615d6
Dr-wandera <wanderafaustine@gmail.com>
2026-04-07 10:00:21 +0300

    added validation to variable

--- src/main/java/com/wanderaTech/auth_service/Model/Users.java
+++ src/main/java/com/wanderaTech/auth_service/Model/Users.java
@@ -2,6 +2,9 @@ package com.wanderaTech.auth_service.Model;
 
 import com.wanderaTech.auth_service.Model.Enum.Role;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,11 +22,18 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String userId;
+    @NotBlank(message = "Field required")
     private String firstName;
+    @NotBlank(message = "Field required")
     private String lastName;
+    @Email
+    @Column(name = "email", unique = true)
+    @NotBlank(message = "Email required")
     private String email;
     private LocalDate createdAt;
+    @NotBlank(message ="Password required" )
     private String password;
+    @NotBlank(message = "Field required")
     private  String phoneNumber;
     @Enumerated(EnumType.STRING)
     private Role role;
