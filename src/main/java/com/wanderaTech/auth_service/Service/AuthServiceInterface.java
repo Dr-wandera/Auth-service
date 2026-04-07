f84302d6c7203eec7ade765da14d6a80f411a800
Dr-wandera <wanderafaustine@gmail.com>
2026-04-07 09:40:55 +0300

    removed unused imports

--- src/main/java/com/wanderaTech/auth_service/Service/AuthServiceInterface.java
+++ src/main/java/com/wanderaTech/auth_service/Service/AuthServiceInterface.java
@@ -2,7 +2,10 @@ package com.wanderaTech.auth_service.Service;
 
 import com.wanderaTech.auth_service.AuthDto.LoginRequest;
 import com.wanderaTech.auth_service.AuthDto.RegisterRequest;
+import com.wanderaTech.auth_service.AuthDto.UserResponse;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface AuthServiceInterface {
     void registerUser(RegisterRequest registerRequest);
@@ -11,4 +14,5 @@ public interface AuthServiceInterface {
 
     long getTotalCustomers();
 
+   List<UserResponse> AllUser(int page, int size);
 }
