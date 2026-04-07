0b1575abef938379981504206cd2fc70459cfca5
Dr-wandera <wanderafaustine@gmail.com>
2026-04-07 09:59:33 +0300

    added UserResponse

---
+++ src/main/java/com/wanderaTech/auth_service/AuthDto/UserResponse.java
@@ -0,0 +1,14 @@
+package com.wanderaTech.auth_service.AuthDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponse {
+    private String userId;
+    private String firstName;
+    private String email;
+}
