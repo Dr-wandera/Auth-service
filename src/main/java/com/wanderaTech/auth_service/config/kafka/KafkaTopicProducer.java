2b91410cc8d1532348608b6524873ab84a3870eb
Dr-wandera <wanderafaustine@gmail.com>
2026-05-16 15:27:33 +0300

    changed class name to meet the application requirement

--- src/main/java/com/wanderaTech/auth_service/config/kafka/KafkaTopicProducer.java
+++ src/main/java/com/wanderaTech/auth_service/config/kafka/KafkaTopicProducer.java
@@ -1,4 +1,4 @@
-package com.wanderaTech.auth_service.KafkaConfig;
+package com.wanderaTech.auth_service.config.kafka;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
