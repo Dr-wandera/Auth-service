de0c08072d40fc7dcb13ebfa492583d552f4bb34
Dr-wandera <wanderafaustine@gmail.com>
2026-05-16 15:36:46 +0300

    added Serialization implementation to UserResponse to meet application requirement

--- src/main/java/com/wanderaTech/auth_service/AuthDto/UserResponse.java
+++ src/main/java/com/wanderaTech/auth_service/AuthDto/UserResponse.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserResponse {
+public class UserResponse implements Serializable {
     private String userId;
     private String firstName;
     private String email;
